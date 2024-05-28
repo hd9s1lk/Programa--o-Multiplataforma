0730762be4a17a496cc222e92e25d8f7e1ea08d2
hd9s1lk <hd9business@gmail.com>
2024-05-28 18:17:32 +0100

    continuacao_aula11

--- Aula_11_LerXAML/Aula_11_LerXAML/MainWindow.xaml.cs
+++ Aula_11_LerXAML/Aula_11_LerXAML/MainWindow.xaml.cs
@@ -25,6 +25,13 @@ namespace Aula_11_LerXAML
             A = (App)App.Current;
 
             A.ModelInscricoes.AlunosChanged += ModelInscricoes_AlunosChanged;
+            A.ModelInscricoes.FicheiroGuardado += ModelInscricoes_FicheiroGuardado;
+        }
+
+        private void ModelInscricoes_FicheiroGuardado()
+        {
+            MessageBox.Show("Ficheiro guardado com sucesso!", "Informação", MessageBoxButton.OK,
+                MessageBoxImage.Information);
         }
 
         private void ModelInscricoes_AlunosChanged()
@@ -100,6 +107,50 @@ namespace Aula_11_LerXAML
             {
                 //chamar os model para guardar os dados que ter armazenados em memória
                 A.ModelInscricoes.SaveJson(saveFile.FileName);
+            }
+        }
+
+        private void AbrirXml_CLICK(object sender, RoutedEventArgs e)
+        {
+            //Permitir ao user selecionar o ficheiro que quer ler
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            //Filtrar os ficheiros
+            //1->0 o que o user vê
+            //2-> Filtro
+            openFile.Filter = "Ficheiro XML|*.xml";
+
+            //Abrir a janela
+            if (openFile.ShowDialog() == true)
+            {
+                //chamar o model para ler o ficheiro selecionado
+                //Guardar os dados em memória
+                A.ModelInscricoes.LerXml(openFile.FileName);
+
+            }
+        }
+
+        private void GuardarTxt_CLICK(object sender, RoutedEventArgs e)
+        {
+            //Guardar um ficheiro
+            SaveFileDialog saveFile = new SaveFileDialog();
+
+            saveFile.Filter = "Ficheiro Txt|*.txt";
+
+            if (saveFile.ShowDialog() == true)
+            {
+                //chamar os model para guardar os dados que ter armazenados em memória
+                A.ModelInscricoes.GuardarTxt(saveFile.FileName);
+            }
+        }
+
+        private void tvAlunos_SELECTEDITEMCHANGED(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var item = tvAlunos.SelectedValue;
+
+            if(item != null)
+            {
+                A.ModelInscricoes.AlterarInscricao(item.ToString().Split(" ; ")[0]);
             }
         }
     }
--- Aula_11_LerXAML/Aula_11_LerXAML/ModelInscricoes.cs
+++ Aula_11_LerXAML/Aula_11_LerXAML/ModelInscricoes.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -20,6 +21,8 @@ namespace Aula_11_LerXAML
         public Dictionary<string,Aluno> Alunos { get; set; }
 
         public event MetodosSemParametros AlunosChanged;
+
+        public event MetodosSemParametros FicheiroGuardado;
 
         public ModelInscricoes() {
             Alunos = new Dictionary<string,Aluno>();
@@ -120,6 +123,116 @@ namespace Aula_11_LerXAML
             string jsonString = JsonSerializer.Serialize(jsonData); 
 
             File.WriteAllText(fileName, jsonString);
+        }
+
+        public void LerXml(string fileName)
+        {
+            //Limpar a lista atual, evitar duplicados
+            Alunos.Clear();
+
+            //Carregar o doc XML para memória 
+            XDocument doc = XDocument.Load(fileName);
+
+            //Pesquisar os alunos inscritos
+            //LINQ para a consulta
+            //from -> de onde vem a pesquisa
+            //select -> vai retornar a lista (coleção)
+            var alunosInscritos = from alunos in doc.Element("Alunos").Element("Inscritos").Descendants("Aluno") select alunos;
+
+            foreach (var dados in alunosInscritos)
+            {
+                //Converter o XElement Aluno para um objeto do tipo Aluno(class)
+                Aluno aluno = new Aluno(dados.Attribute("Numero").Value, 
+                    dados.Attribute("Nome").Value,
+                    dados.Element("Curso").Value);
+
+
+                //Está dentro da secção inscritos
+                aluno.Inscrito = true;
+
+                Alunos.Add(aluno.Numero, aluno);
+            }
+
+            //Pesquisar os alunos n/inscritos
+            var alunosNInscritos = from alunos in doc.Element("Alunos").Element("NInscritos").Descendants("Aluno") select alunos;
+
+            foreach (var dados in alunosNInscritos)
+            {
+                //Converter o XElement Aluno para um objeto do tipo Aluno(class)
+                Aluno aluno = new Aluno(dados.Attribute("Numero").Value,
+                    dados.Attribute("Nome").Value,
+                    dados.Element("Curso").Value);
+
+
+                //Está dentro da secção inscritos
+                aluno.Inscrito = false;
+
+                Alunos.Add(aluno.Numero, aluno);
+            }
+
+            if (AlunosChanged != null)
+            {
+                AlunosChanged();
+            }
+
+
+        }
+
+        public void GuardarTxt(string fileName)
+        {
+            if(Alunos.Count() == 0)
+            {
+                return; //TODO: lançar uma exceção  
+            }
+
+            StreamWriter writer = new StreamWriter(fileName);
+
+            foreach(var aluno in Alunos.Values)
+            {
+                //Converter cada aluno para string
+                //que corresponde a uma llinha do txt
+
+                string line = aluno.Numero + ";" + aluno.Nome + ";" + aluno.Curso + ";";
+
+                if (aluno.Inscrito)
+                {
+                    line += "Inscrito";
+                } else
+                {
+                    line += "NInscrito";
+                }
+
+                writer.WriteLine(line);
+            }
+
+            writer.Close();
+
+            //lançar um evento a informar se o ficheiro foi guardado
+            if(FicheiroGuardado!= null)
+            {
+                FicheiroGuardado();
+            }
+        }
+
+        public void AlterarInscricao(string numero)
+        {
+            //Obter o aluno através do numero
+            //Key -> numero
+            //Retorna o value -> aluno
+            Alunos.TryGetValue(numero, out var aluno);
+
+            if(aluno != null)
+            {
+                //alterar a inscrição do aluno para o contrário
+                aluno.Inscrito = !aluno.Inscrito;
+            }
+
+            if(AlunosChanged!= null)
+            {
+                AlunosChanged();
+            }
+
+
         }
     }
 
